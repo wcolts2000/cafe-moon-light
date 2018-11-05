78c1151f7f38d7402937051dceb3962a8b8210e6
wcolts2000 <wcolts2000@gmail.com>
2018-11-04 23:16:27 -0800

    added card grid component, card component, searchbar component, starting state on navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import DishGrid from './components/DishGrid';
+import SearchBar from './components/SearchBar';
+import data from "./dummyData";
 import './App.css';
 
 class App extends Component {
+  state = {
+    filterTerm: '',
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      filterTerm: event.target.value
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <Header />
+        <SearchBar value={this.state.filterTerm}
+        handleInputChange={this.handleInputChange}
+        />
+        <DishGrid data={data} />
       </div>
-    )
+    );
   }
 }
 
